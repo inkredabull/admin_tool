633a57024bf2ed5ea53b0511947e665f7682062c
Anthony Bull <anthony@bluxomelabs.com>
2013-07-21 19:47:15 -0700

    Update of integration spec

--- test/integration/basic.spec.js
+++ test/integration/basic.spec.js
@@ -1,5 +1,9 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+describe("Index page", function () {
+  it("contains expected elements", function () {
+    visit('/').then(function () {
+      find('.well').length.should.equal(3);
+      find('.span3').length.should.equal(1);
+      find('.span9').length.should.equal(1);
+    });
   });
 });
